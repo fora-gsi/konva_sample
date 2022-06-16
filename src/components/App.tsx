5e1c0e61cae2b110ebda94cb91415cd5aa9661a0
fora-soma <soma@for-a.co.jp>
2022-06-16 22:06:15 +0900

    RegionsList実装（仮
    リージョンの部分をdnd_kitのサンプルで置き換えた形で実装
    ドラッグアンドドロップで順番入れ替えの機能だけ実現

---
+++ src/components/App.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Canvas from "./Canvas";
+import RegionsList from "./RegionsList";
+
+function App() {
+  return (
+    <React.Fragment>
+      <h2>Image annotate prototype</h2>
+      <p className="description">Draw objects contours on top of the image</p>
+      <div className="App">
+        <div className="left-panel">
+          <label>
+            Brightness
+            <input
+              id="slider"
+              type="range"
+              min="-1"
+              max="1"
+              step="0.05"
+              defaultValue="0"
+            />
+          </label>
+          <RegionsList />
+        </div>
+        <div className="right-panel">
+          <Canvas />
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
