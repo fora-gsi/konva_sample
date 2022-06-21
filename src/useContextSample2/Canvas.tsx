7559c3261d1890545cf46502a6a12a58318bfa9c
smark-zk <90439694+fora-soma@users.noreply.github.com>
2022-06-21 09:30:36 +0900

    Spriteのドラッグ対応

--- src/useContextSample2/Canvas.tsx
+++ src/useContextSample2/Canvas.tsx
@@ -1,40 +1,38 @@
-import React, { useContext } from "react";
-import { Layer, Rect, Stage, Text } from "react-konva";
+import { useContext } from "react";
+import { Group, Layer, Rect, Stage, Text } from "react-konva";
 import { UserStore } from "./App";
 
 const Canvas = () => {
   const { sprites } = useContext(UserStore);
   return (
-    <React.Fragment>
-      <Stage width={500} height={500}>
-        <Layer opacity={0.5} style={{ border: "1px solid black" }}>
-          {sprites.map(({ id, x, y, width, height, color, displayName }) => (
-            <React.Fragment key={id.toString()}>
-              <Rect
-                x={x}
-                y={y}
-                width={width}
-                height={height}
-                fill={color}
-                stroke="black"
-              />
-              <Text
-                x={x}
-                y={y}
-                width={width}
-                height={height}
-                fill="black"
-                text={displayName}
-                fontSize={18}
-                fontFamily="Arial"
-                align="center"
-                verticalAlign="middle"
-              />
-            </React.Fragment>
-          ))}
-        </Layer>
-      </Stage>
-    </React.Fragment>
+    <Stage width={500} height={500}>
+      <Layer opacity={0.5} style={{ border: "1px solid black" }}>
+        {sprites.map(({ id, x, y, width, height, color, displayName }) => (
+          <Group draggable={true} key={id.toString()}>
+            <Rect
+              x={x}
+              y={y}
+              width={width}
+              height={height}
+              fill={color}
+              stroke="black"
+            />
+            <Text
+              x={x}
+              y={y}
+              width={width}
+              height={height}
+              fill="black"
+              text={displayName}
+              fontSize={18}
+              fontFamily="Arial"
+              align="center"
+              verticalAlign="middle"
+            />
+          </Group>
+        ))}
+      </Layer>
+    </Stage>
   );
 };
 
