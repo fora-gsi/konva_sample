ffcc79d9fe525d10f8c83cd71bb2b55050d792e3
smark-zk <90439694+fora-soma@users.noreply.github.com>
2022-06-23 14:13:06 +0900

    ファイル整理

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import App from "./components/App";
 // import App from "./dnd_kit_sample/App";
 // import App from "./useContextSample/App";
 // import App from "./useContextSample2/App";
-// import App from "./usecontext_test/App";
+// import App from "./useContextSample3/App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
