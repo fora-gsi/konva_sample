43cec28172cd677820611bbc7d5b76866baa0a42
smark-zk <90439694+fora-soma@users.noreply.github.com>
2022-07-15 17:03:01 +0900

    射影変換における入隅検知のアプローチを変更
    homographyのサンプルにおいて、入隅検知方法のアプローチを変更。
    
    操作中の角の角度計算をやめて
    線分と円による衝突検知を実施。

--- src/homography/App.tsx
+++ src/homography/App.tsx
@@ -1,8 +1,10 @@
-import React, { useCallback, useEffect, useState } from "react";
+import Konva from "konva";
+import React, { useState } from "react";
 import { Circle, Group, Layer, Line, Stage } from "react-konva";
 import Vector2d from "./Vector2d";
 
-const pointerRad = 5;
+const pointerRad = 5; // 画面上に登場する変形用のハンドル
+const hitArea = 0.5; // 線分と当たり判定を行う円の半径
 const startX = 10;
 const startY = 10;
 const rectWidth = 320;
@@ -15,53 +17,67 @@ const initialPoints: Vector2d[] = [
 ];
 
 /**
- * 座標A,B,Cを入力し、∠ABCのなす角度をラジアンで返す。
- * 座標ABCは反時計回りで入力すること。
+ * 点と直線の距離を返す関数
  *
- * @param a 座標A
- * @param b 座標B
- * @param c 座標C
- * @returns ∠ABCの成す角度
+ * @param point
+ * @param startCoord
+ * @param endCoord
+ *
+ * @return 点と直線の距離
  */
-const calcAngle = (a: Vector2d, b: Vector2d, c: Vector2d): number => {
-  const va: Vector2d = Vector2d.sub(a, b);
-  const vb: Vector2d = Vector2d.sub(c, b);
+const distance = (
+  point: Vector2d,
+  startCoord: Vector2d,
+  endCoord: Vector2d
+): number => {
+  const v = new Vector2d(endCoord.x - startCoord.x, endCoord.y - startCoord.y); // 線分
+  const v1 = new Vector2d(point.x - startCoord.x, point.y - startCoord.y); // 線分の始点からマウスポジションを向くベクトル
 
-  // ベクトルの内積をとる
-  const dot = Vector2d.dot(va, vb);
+  const d = Vector2d.cross(v.normalized, v1); // 線分と円の中心の最短距離
+  return d;
+};
 
-  // dot = |a|*|b|cosθという公式を変形し
-  // cosθ = dot/|a|*|b|としてcosθを導く
-  const cosTheta = dot / (va.magnitude * vb.magnitude);
+const pushBack = (pa: Vector2d, pb: Vector2d, distance: number) => {
+  const va = new Vector2d(pb.x - pa.x, pb.y - pa.y); // 点AからBを指すベクトル
+  const vu = new Vector2d(-va.y, va.x).normalized.times(distance);
 
-  // cosθから角度θを求める
-  const theta = Math.acos(cosTheta);
-
-  // ベクトルの外積をとる
-  const cross = Vector2d.cross(va, vb);
-
-  // 外積のz成分が0未満なら角は出隅である
-  if (cross < 0) {
-    return theta;
-  } else {
-    return 2 * Math.PI - theta;
-  }
+  return vu;
 };
 
 export default function App() {
   const [coords, setCoords] = useState<Vector2d[]>(initialPoints);
-  const [angle, setAngle] = useState(0);
+  const circleRefs = React.useRef<Konva.Circle[]>([]);
 
-  const rad2deg = (rad: number) => rad * (180 / Math.PI);
+  function cornerDragging(idx: number) {
+    const circle = circleRefs.current[idx];
+    if (!circle) {
+      return;
+    }
+    let circlePos = new Vector2d(
+      circle.absolutePosition().x,
+      circle.absolutePosition().y
+    );
 
-  const handleAngleUpdate = useCallback(() => {
-    const tmpAngle = rad2deg(calcAngle(coords[3], coords[2], coords[1]));
-    setAngle(tmpAngle);
-  }, [coords]);
+    let startCoord = coords[idx - 1 >= 0 ? idx - 1 : coords.length - 1];
+    let endCoord = coords[idx + 1 < coords.length ? idx + 1 : 0];
 
-  useEffect(() => {
-    handleAngleUpdate();
-  }, [coords, handleAngleUpdate]);
+    const d = distance(circlePos, startCoord, endCoord);
+    if (d < hitArea) {
+      circlePos.add(pushBack(startCoord, endCoord, hitArea - d));
+    }
+    setCoords((coords) =>
+      coords.map((value, i) =>
+        idx === i
+          ? new Vector2d(
+              circle.absolutePosition().x,
+              circle.absolutePosition().y
+            )
+          : value
+      )
+    );
+
+    circleRefs.current[idx].absolutePosition(circlePos);
+  }
 
   return (
     <React.Fragment>
@@ -93,20 +109,16 @@ export default function App() {
                 strokeWidth={2}
                 draggable
                 onDragMove={(e) => {
-                  setCoords((coords) =>
-                    coords.map((value, idx) =>
-                      idx === i
-                        ? new Vector2d(e.target.x(), e.target.y())
-                        : value
-                    )
-                  );
+                  cornerDragging(i);
+                }}
+                ref={(circle) => {
+                  if (circle) circleRefs.current.push(circle);
                 }}
               />
             ))}
           </Group>
         </Layer>
       </Stage>
-      <div id="pAngle3">右下の角度：{Math.round(angle * 100) / 100}°</div>
     </React.Fragment>
   );
 }
